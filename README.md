79cedc3de67220ff0390eec45d1dab18bd5bb410
Carla <carlaheywood24@gmail.com>
2023-05-19 18:52:16 -0700

    Updated BulkEmailer.py

---
+++ README.md
@@ -0,0 +1,33 @@
+# BulkEmailer.py
+
+BulkEmailer.py is a simplified email blaster. Enter your Gmail, upload the email template you would like to send out, then upload your CSV email list. Sending hunrededs of emails without ending up in the junk folder made quick and easy.
+
+<a href="">Demo Link</a>
+
+<img src="" width="100%">
+
+## Set up
+
+Confirgure Flask-Mail:
+Create an app specific password to login - use the option Other(Custom Name).
+Paste in this password with your username in the code.
+https://myaccount.google.com/apppasswords
+
+## Running
+
+```bash
+pip install -r requirements.txt
+virtualenv env
+source env/bin/activate
+python emailer.py
+```
+
+Confirm results in browser using http://127.0.0.1:5000/
+
+## Contributing
+
+A coworker of mine, Justin, inspired me to get this project going. He had the idea to send out emails to our clients using the BCC feature on Gmail. I tried this for some emails and found that this method is ineffective. Mostly showing up in the junk folder or getting blocked due to hidden recipents. There are similar paid services for this like MailChimp and Constant Contact, but I knew I could devlop something simple and tailored to our needs. Here is the extremely simple email blaster, BulkEmailer.py - Developed by Carla Heywood.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ emailer.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from flask import Flask, render_template, request, render_template_string
+from flask_mail import Mail, Message
+import pandas as pd
+import csv
+import os
+
+app = Flask(__name__)
+
+# Flask-Mail configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'carlaheywood24@gmail.com'
+app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_SECRET')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEFAULT_SENDER'] = ('BulkEmailer.py', 'BulkEmailer.py@noreply.com')
+
+mail = Mail(app)
+
+# Variables for templates
+today = datetime.now()
+day_name = today.strftime("%A")
+current_year = today.year
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html', current_year=current_year)
+
+@app.route('/sendBulkEmail', methods=['POST'])
+def sendBulkEmail():
+    if request.method == 'POST':
+        csv_file = request.files.get('csv')
+        html_file = request.files.get('html')
+        confirmation_email = request.form.get('confirmation_email')
+        sent=0
+
+        if csv_file:
+            df = pd.read_csv(csv_file)
+            emails = df['Email'].tolist()
+            sent=len(emails)
+
+            if html_file:
+                html_content = html_file.read().decode('utf-8')
+            else:
+                html_content = "Hello World! default message from BulkEmailer.py"
+
+            for email in emails:
+                msg = Message('Hello, Happy ' + day_name + '! from BulkEmailer.py', recipients=[email])
+                msg.html = render_template_string(html_content, day_name=day_name, current_year=current_year)
+                mail.send(msg)
+
+            # Send confirmation email
+            msg = Message("BulkEmailer.py Confirmation // " + str(sent) + " Email(s)", recipients=[confirmation_email])
+            msg.body = "BulkEmailer.py Complete.\n\n" + str(sent) + " Emails Sent successfully.\n\n"
+            if csv_file:
+                msg.attach(csv_file.filename, 'text/csv', csv_file.read())
+            if html_file:
+                msg.attach(html_file.filename, 'text/html', html_file.read())
+            mail.send(msg)
+
+    return render_template('success.html')
+
+@app.route('/send_single_email', methods=['POST'])
+def send_single_email():
+    email = request.form.get('email')
+
+    msg = Message('Hi there, it\'s Carla!', sender = ('Carla Heywood', 'carlaheywood24@gmail.com'), recipients = [email])
+    msg.body = 'Hello, Happy ' + day_name + '! \n\nThank you for checking out my project. \nThis message was sent in ' + current_year + ' from BulkEmailer.py using Flask Mail. \n\nMake it a great day!\ncarlaheywood24@gmail.com"'
+    mail.send(msg)
+
+    return render_template('success.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
